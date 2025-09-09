26b7aa877b9e1c9f51ab31829b24b84b1176fd9f
dima <dima.kuzov@gmail.com>
2025-09-09 11:38:49 +0200

    fix edit position button

--- js/panelEditor.js
+++ js/panelEditor.js
@@ -151,6 +151,7 @@ export class PanelEditor {
         this.editButtons.appendChild(saveBtn);
         this.editButtons.appendChild(deleteBtn);
         this.editButtons.appendChild(editPositionBtn);
+        this.editPositionBtn = editPositionBtn;
 
         document.body.appendChild(this.editButtons);
     }
